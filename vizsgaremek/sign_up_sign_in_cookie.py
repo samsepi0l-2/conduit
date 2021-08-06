c4237478d40f8c479330be679f44193eefb670a0
Budai Jozsef <bujoci@gmail.com>
2021-08-06 11:37:07 +0200

    working in progress

--- vizsgaremek/sign_up_sign_in_cookie.py
+++ vizsgaremek/sign_up_sign_in_cookie.py
@@ -1,5 +1,6 @@
 import secrets
 import time
+import csv
 
 from selenium import webdriver
 import os.path
@@ -68,8 +69,7 @@ assert cookie_accept["value"] == "accept"
 
 time.sleep(2)
 
-
-#real registration
+# real registration
 username_reg_field = browser.find_element_by_xpath("//input[@placeholder='Username']")
 email_reg_field = browser.find_element_by_xpath("//input[@placeholder='Email']")
 password_reg_field = browser.find_element_by_xpath("//input[@placeholder='Password']")
@@ -97,7 +97,7 @@ navbar_all_item_logged_in[4].click()
 session_cookie = browser.get_cookie("drash_sess")
 assert session_cookie["value"] == "null"
 
-#login
+# login
 navbar_all_item_logged_out = browser.find_elements_by_xpath("//li[@class= 'nav-item']")
 navbar_all_item_logged_out[1].click()
 
@@ -124,7 +124,7 @@ time.sleep(1)
 assert browser.get_cookie("drash_sess")["value"] != "null"
 
 navbar_all_item_logged_in = browser.find_elements_by_xpath("//li[@class= 'nav-item']")
-# usernam ellenérzés a random adatoknál
+# username ellenérzés a random adatoknál
 # assert navbar_all_item_logged_in[3].text == username_good_format
 
 # username ellenőrzés az átmeneti égetett adatokkal
@@ -150,6 +150,21 @@ time.sleep(2)
 article_url = browser.current_url
 assert article_url == "http://conduitapp.progmasters.hu:1667/#/articles/titleteszt3"
 
+# edit article
+
+edit_btn = browser.find_element_by_xpath("//a[contains(@href,'editor/titleteszt3')]")
+edit_btn.click()
+time.sleep(1)
+article_body_field = browser.find_element_by_xpath("//textarea[@placeholder='Write your article (in markdown)']")
+article_body_field.send_keys(" EDITED")
+article_publish_btn = browser.find_element_by_xpath("//button[@class= 'btn btn-lg pull-xs-right btn-primary']")
+article_publish_btn.click()
+time.sleep(1)
+
+edited_contetnt = browser.find_element_by_xpath("//div[@class = 'row article-content']/div/div/p")
+assert edited_contetnt.text == "bodyteszt EDITED"
+browser.back()
+
 # delete article
 navbar_all_item_logged_in = browser.find_elements_by_xpath("//li[@class= 'nav-item']")
 navbar_all_item_logged_in[0].click()
@@ -160,13 +175,14 @@ for i in titles_before_delete:
     before_delete_list.append(i.text)
 
 browser.back()
+browser.back()
 time.sleep(1)
 
 delete_btn = browser.find_element_by_xpath("//button[@class='btn btn-outline-danger btn-sm']")
 delete_btn.click()
 
-delete_msg = browser.find_element_by_xpath("//div[@class='swal-overlay']/div/div")
-print(delete_msg.text)
+# delete_msg = browser.find_element_by_xpath("//div[@class='swal-overlay']/div/div")
+# print(delete_msg.text)
 time.sleep(2)
 titles_after_delete = browser.find_elements_by_xpath("//a[@class='preview-link']/h1")
 after_delete_list = []
@@ -176,9 +192,79 @@ for i in titles_after_delete:
 difference = []
 deleted_title = ""
 for i in before_delete_list:
-  if i not in after_delete_list:
-    deleted_title = deleted_title + i
+    if i not in after_delete_list:
+        deleted_title = deleted_title + i
 
-assert  deleted_title == "titleteszt3"
-time.sleep(5)
+assert deleted_title == "titleteszt3"
+
+# data from file
+#       new post
+navbar_all_item_logged_in = browser.find_elements_by_xpath("//li[@class= 'nav-item']")
+navbar_all_item_logged_in[1].click()
+time.sleep(1)
+article_title_field = browser.find_element_by_xpath("//input[@placeholder='Article Title']")
+article_about_field = browser.find_element_by_xpath("//input[contains(@placeholder,'this article about')]")
+article_body_field = browser.find_element_by_xpath("//textarea[@placeholder='Write your article (in markdown)']")
+article_tag_field = browser.find_element_by_xpath("//input[@placeholder='Enter tags']")
+article_publish_btn = browser.find_element_by_xpath("//button[@class= 'btn btn-lg pull-xs-right btn-primary']")
+
+article_title_field.send_keys("commentitle")
+article_about_field.send_keys("commentabout")
+article_body_field.send_keys("commnetbody")
+article_tag_field.send_keys("commentag")
+article_publish_btn.click()
+
+time.sleep(2)
+comment_field = browser.find_element_by_xpath("//textarea[@placeholder='Write a comment...']")
+comment_btn = browser.find_element_by_xpath("//div[@class= 'card-footer']/button")
+
+with open("input_data.txt", "r") as file:
+    comment_lines = file.readlines()
+    for i in comment_lines:
+        comment = i.strip()
+        comment_field.send_keys(comment)
+        comment_btn.click()
+        time.sleep(1)
+
+comment_list = browser.find_elements_by_xpath("//p[@class = 'card-text']")
+stripped_list = []
+text_list = []
+for i in range(len(comment_lines) - 1, -1, -1):
+    stripped_list.append(comment_lines[i].strip())
+
+for k in comment_list:
+    text_list.append(k.text)
+
+# ciklusok nélkül
+# assert comment_list[0].text == comment_lines[2].strip() and comment_list[1].text == comment_lines[1].strip() and \
+#        comment_list[2].text == comment_lines[0].strip()
+
+assert stripped_list == text_list
+
+delete_btn = browser.find_element_by_xpath("//button[@class='btn btn-outline-danger btn-sm']")
+delete_btn.click()
+time.sleep(2)
+
+# data to file
+authors = browser.find_elements_by_xpath("//a[@class = 'author']")
+titles = browser.find_elements_by_xpath("//a[@class = 'preview-link']/h1")
+summaries = browser.find_elements_by_xpath("//a[@class = 'preview-link']/p")
+likes = browser.find_elements_by_xpath("//span[@class = 'counter']")
+# header = ['author', 'title', 'summary', 'number_of_likes']
+
+with open('output_data.csv', 'w', encoding='utf-8') as new_csv:
+    # writer = csv.writer(new_csv)
+    # writer.writerow(header)
+    new_csv.write("author" + "," + "title" + "," + "summary" + "," + "number_of_likes" + "\n")
+    for i in range(len(authors) - 1):
+        new_csv.write(authors[i].text + "," + titles[i].text + "," + summaries[i].text + "," + likes[i].text + "\n")
+
+with open('output_data.csv', 'r', encoding="utf-8") as file:
+    reader = csv.reader(file, delimiter=',')
+    rows = list(file)
+
+    print(rows[8])
+    print(titles[7].text)
+
+time.sleep(15)
 browser.quit()
